2a841eeb7657a8ebd73c3bb43341c136b8f222f6
Wojciech Dzikowski <dzikowskw@gmail.com>
2018-04-18 00:23:09 +0200

    Websockets + dummy dev server serving webscokets

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,12 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import serverPush from './services/serverPush';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
+serverPush.initSocket();
+
---
+++ src/services/serverPush.ts
@@ -0,0 +1,13 @@
+import * as io from 'socket.io-client';
+
+let socket;
+
+function initSocket(url='http://localhost:8000') {
+  socket = io(url);
+  socket.on('connect', ()=> console.log('SOCKET CONNECTED!')); // tslint:disable-line
+  return socket;
+}
+
+export default {
+  initSocket,
+};
