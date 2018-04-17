16dc95e80429f1327647415f78a0c4e7536ab852
Wojciech Dzikowski <dzikowskw@gmail.com>
2018-04-17 19:05:11 +0200

    Added bulma, changed package name

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import 'bulma/css/bulma.css';
+import 'bulma/css/bulma.css';
 import * as React from 'react';
-import './App.css';
 import CmdInput from './components/CmdInput/CmdInput';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Hipchat command tester</h1>
-        </header>
-        <CmdInput />
-      </div>
+      <section className="section">
+        <div className="container">
+          <h1 className="title">
+            Hello World
+          </h1>
+          <p className="subtitle">
+            My first website with <strong>Bulma</strong>!
+            <CmdInput />
+          </p>
+        </div>
+      </section>
     );
   }
 }
