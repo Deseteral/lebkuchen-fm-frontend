cb889be3758fed72ff5948374d34a3d4f06f597a
Wojciech Dzikowski <dzikowskw@gmail.com>
2018-04-18 16:41:24 +0200

    bugfix

--- src/services/serverPush.ts
+++ src/services/serverPush.ts
@@ -44,7 +44,7 @@ function handleX(xsound: IXMessage) {
 }
 
 function initSocket(url='http://localhost:8000') {
-  socket = io(url);
+  socket = io();
   socket.on('connect', ()=> console.log('SOCKET CONNECTED!')); // tslint:disable-line
   socket.on('queue', handleIncomingMsg);
   socket.on('say', handleSay);
