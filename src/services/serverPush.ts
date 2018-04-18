8399342a72569a05c94a199fd449135efc7d725e
Michał Ferchow <deseteral@gmail.com>
2018-04-18 08:35:05 +0200

    log2

--- src/services/serverPush.ts
+++ src/services/serverPush.ts
@@ -5,7 +5,7 @@ let socket;
 function initSocket(url='http://localhost:8000') {
   socket = io(url);
   socket.on('connect', ()=> console.log('SOCKET CONNECTED!')); // tslint:disable-line
-  socket.on('queue', (song) => console.log(song)); // tslint:disable-line
+  socket.on('queue', (song) => console.log(song: any)); // tslint:disable-line
   return socket;
 }
 
