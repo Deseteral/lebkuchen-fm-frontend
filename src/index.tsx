2df39f435c82291faa528e1d9ea91593b8d0e04b
Wojciech Dzikowski <dzikowskw@gmail.com>
2018-04-18 09:09:34 +0200

    Removed service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 import serverPush from './services/serverPush';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
 serverPush.initSocket();
-
